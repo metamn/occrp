a5ef42a48a24e735bc6a20ebcad86fc47cc3857b
metamn <bartus.csongor@gmail.com>
2013-12-12 17:07:18 +0200

    Isotope still not working ....

--- _site/assets/jquery.occrp.js
+++ _site/assets/jquery.occrp.js
@@ -1,25 +1,13 @@
 
 $(document).ready(function() {
-  // The isotope cache container
-  var $container = $('#investigations');
-  
-  // The window onload script
+ 
+ 
+ // The window onload script
   // - here we put all scripts which must run after the page is completelly loaded
   $(window).load(function(){
-    
-    // Initialize isotope filter plugin
-    // Set options  
-    $container.isotope({
-      itemSelector : 'article',
-      
-      // The CSS animation was too fancy, we've switched to a simpler one
-      animationEngine: 'jquery',
-      animationOptions: {
-         duration: 750,
-         easing: 'linear',
-         queue: false
-       }
+    $('#investigations').isotope({ 
+      itemSelector : 'investigations',
+      layoutMode : 'fitRows' 
     });
   });
-  
 });
